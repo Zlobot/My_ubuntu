67d22fe8532a3ad9d727502f2f3000b2d1ccefbd
Zlobot <zlobot@ymail.com>
2019-08-30 16:41:53 +0300

    I have made some changes to binary search

--- mx_strcmp.c
+++ mx_strcmp.c
@@ -10,63 +10,51 @@ int mx_strcmp(char* a,const char* b){
 }
 
 int mx_binary_search(char **arr, int size, const char *s, int *count){
-	int mid = size / 2;
-	int left = mid - 1;
-	int right = mid;
+	int start = 0;
+	int end = size - 1;
 	int search = 0;
-
-	if (mx_strcmp(arr[size - 1], s) < 0)
+	int mid;
+	mid = (start + end) / 2;
+	/*if (mx_strcmp(arr[mid],s) == 0){
+		search+= 1;
+		*count = search;
+		return mid;
+	}*/
+	if (mx_strcmp(arr[size - 1],s) >= 0) {
+		while (end > start) {
+			search++;
+			*count = 2121;
+			if (mx_strcmp(arr[mid],s) == 0) {
+				*count = 911;
+				return mid;
+			}
+			else if (mx_strcmp(arr[mid],s) < 0) {
+				start = mid + 1;
+				mid = (start + end) / 2;
+			}
+			else {
+				end = mid -1;
+				mid = (start + end) / 2;
+			}
+		}
+	}
+	else
 		return -1;
 
-	else if (mx_strcmp(arr[size - 1], s) == 0) {
-		return size - 1;
-	}
-	else {
-		search+= 1;
-		if (mx_strcmp(arr[left], s) == 0) {
-			*count = search;
-			return left;
-		}
-		else if (mx_strcmp(arr[left], s) > 0) {
-			left = left / 2;
-			// left == 2;
-			search+= 1;
-			if (mx_strcmp(arr[left], s) == 0) {
-				*count = search;
-				return left;
-			}
-			else if (mx_strcmp(arr[left], s) > 0) {
-				left = left / 2;
-				// left == 1;
-				search+= 1;
-				if (mx_strcmp(arr[left], s) == 0) {
-					*count = search;
-					return left;
-				}
-				else if (mx_strcmp(arr[left], s) > 0) {
-					left = left / 2;
-					//left = 0;
-					search+= 1;
-					if (mx_strcmp(arr[left], s) == 0) {
-						*count = search;
-						return left;
-					}
-				}
-			}
-		}
-		else
-			return 911;
-	}
 }
 
 int main(){
 	char a[] = "AAAA";
 	char b[] = "B";
 	int count = 0;
-	int size = 10;
-	char* text = "1";
-	char * massiv_sliv[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	int size = 7;
+	char* text = "6";
+	char * massiv_sliv[7] = {"0", "1", "2", "3", "4", "5", "6"};
 
-	printf("%i\n", mx_binary_search(massiv_sliv, size, text, &count));
-	printf("%i\n", count);
+	int answer;
+	answer = mx_binary_search(massiv_sliv, size, text, &count);
+	mx_binary_search(massiv_sliv, size, text, &count);
+
+	printf("%i\n%i\n", answer, count);
+	int len = sizeof(massiv_sliv) / sizeof(massiv_sliv[0]);
 }
