ba1e495c320b38844b841b90a3ddad472474ee81
Zlobot <zlobot@ymail.com>
2019-09-05 12:26:42 +0300

    I probably have managed to solve Binary search task

--- mx_strcmp.c
+++ mx_strcmp.c
@@ -12,49 +12,38 @@ int mx_strcmp(char* a,const char* b){
 int mx_binary_search(char **arr, int size, const char *s, int *count){
 	int start = 0;
 	int end = size - 1;
-	int search = 0;
 	int mid;
 	mid = (start + end) / 2;
-	/*if (mx_strcmp(arr[mid],s) == 0){
-		search+= 1;
-		*count = search;
-		return mid;
-	}*/
-	if (mx_strcmp(arr[size - 1],s) >= 0) {
-		while (end > start) {
-			search++;
-			*count = 2121;
-			if (mx_strcmp(arr[mid],s) == 0) {
-				*count = 911;
+
+	if (strcmp(arr[size - 1],s) >= 0) {
+		while (end >= start) {
+			(*count)++;			
+			if (strcmp(arr[mid],s) == 0)
 				return mid;
-			}
-			else if (mx_strcmp(arr[mid],s) < 0) {
+			else if (strcmp(arr[mid],s) < 0)
 				start = mid + 1;
-				mid = (start + end) / 2;
-			}
-			else {
+			else if (strcmp(arr[mid],s) > 0)
 				end = mid -1;
-				mid = (start + end) / 2;
-			}
+			mid = (start + end) / 2;
 		}
 	}
-	else
-		return -1;
+
+	if (&count > 0)
+		*count = 0;
+	return -1;
 
 }
 
 int main(){
-	char a[] = "AAAA";
-	char b[] = "B";
 	int count = 0;
-	int size = 7;
-	char* text = "6";
-	char * massiv_sliv[7] = {"0", "1", "2", "3", "4", "5", "6"};
+	int size = 5;
+	char* text = "e";
+	char * massiv_sliv[5] = {"a", "b", "c", "d", "j"};
 
 	int answer;
 	answer = mx_binary_search(massiv_sliv, size, text, &count);
-	mx_binary_search(massiv_sliv, size, text, &count);
 
 	printf("%i\n%i\n", answer, count);
-	int len = sizeof(massiv_sliv) / sizeof(massiv_sliv[0]);
+	return 0;
+
 }
